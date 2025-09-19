cf86228998ee66756b591f6c76afdd5cff1ff1a8
detiDevs <>
2025-09-19 13:13:58 +0200

    Set main.py to start SandboxModeWindow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from control.AppControl import AppController
+import sys
+
+from PySide6 import QtWidgets
+
+from view.SandboxModeWindow import SandboxModeWindow
 
 if __name__ == "__main__":
-    controller = AppController()
-    controller.run()
+    app = QtWidgets.QApplication(sys.argv)
+    w = SandboxModeWindow()
+    w.show()
+    sys.exit(app.exec())

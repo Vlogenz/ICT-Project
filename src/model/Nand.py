742f49fffec6e4414d0373ec26c164a91c87c759
Luis Gebhardt <luisgebhardt@gmail.com>
2025-09-20 18:38:23 +0200

    Added comments for readability

--- src/model/Nand.py
+++ src/model/Nand.py
@@ -5,7 +5,9 @@ class Nand(LogicComponent):
 
     def __init__(self):
         super().__init__()
-        self.inputs: typing.Dict = {"input1": None, "input2": None} # NAND gate has exactly two inputs
+        self.inputs: typing.Dict = {"input1": None, "input2": None} 
+        # NAND gate has exactly two inputs
+        #   (Tuples of component and output key of that component)
         self.state: dict = {"outValue": (1,1)}
     
     def eval(self) -> bool:
@@ -15,14 +17,20 @@ class Nand(LogicComponent):
             bool: True if the output state has changed, False otherwise.
         """
         oldState = self.state.copy()
-        if self.inputs["input1"] is None:
+        if self.inputs["input1"] is None: # set input to false if no component is connected
             a = False
         else:
             a = self.inputs["input1"][0].getState()[self.inputs["input1"][1]][0]
-        if self.inputs["input2"] is None:
+            # gets the component out of the first tuple in self.inputs and then 
+            #   uses the key from that tuple to access the right output from the 
+            #   components state
+        if self.inputs["input2"] is None: # set input to false if no component is connected
             b = False
         else:
             b = self.inputs["input2"][0].getState()[self.inputs["input2"][1]][0]
+            # gets the component out of the second tuple in self.inputs and then 
+            #   uses the key from that tuple to access the right output from the 
+            #   components state
         if a and b:
             self.state["outValue"] = (0,1)
         else:

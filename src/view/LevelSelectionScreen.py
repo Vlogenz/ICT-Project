49a1da3d16787d1db7be3dd0456780466bdcb6ea
detiDevs <>
2025-10-27 11:16:15 +0100

    translated stuff to english and create grid

--- src/view/LevelSelectionScreen.py
+++ src/view/LevelSelectionScreen.py
@@ -2,11 +2,22 @@ from PySide6 import QtGui, QtWidgets
 from src.infrastructure.eventBus import getBus
 
 class LevelSelectionScreen(QtWidgets.QWidget):
+    """
+    Shows a grid with a cell for each level.
+    """
+
     def __init__(self, app_controller):
         self.app_controller = app_controller
         self.bus = getBus()
-        # Zeigt Buttons/Karten für jedes Level
+
+        self.grid = QtWidgets.QGridLayout()
+        # load levels
+
+
+
+        
+        # create a cell for each level and put it on the grid
         
     def on_level_clicked(self, level_number: int):
-        """User klickt auf Level"""
+        """User clicks on a level"""
         self.bus.emit("levelSelection:levelSelected", level_number)

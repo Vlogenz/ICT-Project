eee33ee1639622afcadab2ab8202a20eaf40b39b
detiDevs <>
2025-10-30 12:33:14 +0100

    Improved level overview, added levels, changed how meta file works

--- src/view/LevelSelectionWindow.py
+++ src/view/LevelSelectionWindow.py
@@ -14,29 +14,68 @@ class LevelSelectionWindow(QtWidgets.QMainWindow):
         self.levelFileController = levelFileController
         self.bus = getBus()
 
-        # Import metafile, and unlocked metafile
-        metaFile = self.levelFileController.loadMetaFile()
-        self.levelUnlocked = metaFile["level_unlocked"]
-
         central = QtWidgets.QWidget()
         self.layout = QtWidgets.QVBoxLayout(central)
         self.setCentralWidget(central)
-        self.grid = QtWidgets.QGridLayout()
 
         headerLabel = QtWidgets.QLabel("Select a level")
         headerLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.grid = QtWidgets.QGridLayout()
+        self.toggleLevelLockButton = QtWidgets.QPushButton()
+        if self.levelFileController.getAllLevelsUnlocked():
+            self.toggleLevelLockButton.setText("Lock levels")
+        else:
+            self.toggleLevelLockButton.setText("Unlock all levels")
+        self.toggleLevelLockButton.clicked.connect(self.toggleLevelLock)
+
         self.layout.addWidget(headerLabel)
         self.layout.addLayout(self.grid)
+        self.layout.addWidget(self.toggleLevelLockButton)
 
-        # load levels
-        levels = levelFileController.getAvailableLevels()
+        self.levelButtons = []
+        self.createLevelButtons()
 
-        # create a cell for each level and put it on the grid
+    def createLevelButtons(self):
+        """Initialize all level buttons. If a level is done, it gets the 'done' label.
+        If it is locked, it gets the 'locked' label and you cannot open it.
+        """
+        levels = self.levelFileController.getAvailableLevels()
+        completed = self.levelFileController.getCompletedLevels()
+        unlocked = self.levelFileController.getAllLevelsUnlocked()
+        max_completed = max(completed) if completed else -1
+
         for i in levels:
-            button = QtWidgets.QPushButton(f"Level {i}")
-            button.clicked.connect(lambda: self.on_level_clicked(i))
+            button = QtWidgets.QPushButton()
+            text = f"Level {i}"
+            if i in completed:
+                text += " (Done)"
+            elif not unlocked and i > max_completed + 1:
+                text += " (Locked)"
+            button.setText(text)
+            if not (not unlocked and i > max_completed + 1):
+                button.clicked.connect(lambda checked=False, lvl=i: self.onLevelClicked(lvl))
             self.grid.addWidget(button, i//COLUMNS, i%COLUMNS)
-        
-    def on_level_clicked(self, level_number: int):
+            self.levelButtons.append(button)
+
+    def onLevelClicked(self, level_number: int):
         """User clicks on a level"""
         self.bus.emit("levelSelection:levelSelected", level_number)
+
+    def toggleLevelLock(self):
+        """Toggles whether all levels are unlocked.
+        """
+        if not self.levelFileController.getAllLevelsUnlocked():
+            self.levelFileController.setAllLevelsUnlocked(True)
+            self.toggleLevelLockButton.setText("Lock levels")
+        else:
+            self.levelFileController.setAllLevelsUnlocked(False)
+            self.toggleLevelLockButton.setText("Unlock all levels")
+        self.updateLevelButtons()
+
+    def updateLevelButtons(self):
+        """Updates the level buttons."""
+        for b in self.levelButtons:
+            self.grid.removeWidget(b)
+            b.deleteLater()
+        self.levelButtons.clear()
+        self.createLevelButtons()

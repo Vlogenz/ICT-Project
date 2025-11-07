ae73386c3e7b20a8b2e0b561b8e60875c33f3586
detiDevs <>
2025-11-07 09:32:45 +0100

    Move component map to constants and changed palette creation

--- src/view/LogicComponentPalette.py
+++ src/view/LogicComponentPalette.py
@@ -1,15 +1,23 @@
+from symtable import Class
 from PySide6 import QtWidgets, QtCore
+from typing import List, TypeVar, Type
 
 from src.view.PaletteItem import PaletteItem
-from src.constants import PALETTE_COLS
+from src.model import LogicComponent
+from src.constants import PALETTE_COLS, COMPONENT_MAP
 
 class LogicComponentPalette(QtWidgets.QScrollArea):
 
-    def __init__(self, classes):
+    T = TypeVar("T", bound=LogicComponent)
+    def __init__(self, classes: List[Type[T]] = None):
         super().__init__()
+        if classes is not None:
+            classesList = classes
+        else:
+            classesList = COMPONENT_MAP.values()
 
         self.layout = QtWidgets.QGridLayout()
-        for i, class_ in enumerate(classes):
+        for i, class_ in enumerate(classesList):
             # Use index for a x-column grid
             self.layout.addWidget(PaletteItem(class_), i // PALETTE_COLS, i % PALETTE_COLS)
         paletteFrame = QtWidgets.QFrame()
